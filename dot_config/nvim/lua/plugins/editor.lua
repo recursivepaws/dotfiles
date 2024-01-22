73938c2131b6becb3670c9397d94b204f0df4b03
Vera Gonzalez <me@vera.lgbt>
2024-01-22 17:28:21 -0500

    feat: treesitter code folding

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -46,4 +46,16 @@ return {
 		ft = { "rust" },
 		dependencies = { "mfussenegger/nvim-dap" },
 	},
+	-- Pretty LSP-based folding
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = { "kevinhwang91/promise-async" },
+		config = function()
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
 }
--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -22,9 +22,15 @@ return {
 				"javascript",
 				"html",
 			},
+			auto_install = true,
 			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
+			highlight = { enable = true, additional_vim_regex_highlighting = false },
+			ident = { enable = true },
+			rainbow = {
+				enable = true,
+				extended_mode = true,
+				max_file_lines = nil,
+			},
 		})
 	end,
 }
