df5ced3f2c04fa5fbda0482a45639339e7ccadc8
Vera Gonzalez <me@vera.lgbt>
2024-02-15 22:32:09 -0500

    feat: updated terminal loading

---
+++ dot_config/scripts/fedora/root/executable_terminal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+if [ ${EUID} != 0 ]; then
+	echo "This script needs to be run as root"
+	exit 1
+fi
+
+# Install zsh
+dnf install -y git zsh
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Exit new shell
+$(exit)
