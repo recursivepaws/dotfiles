40507839d9dac1ded6ec35990b1ea2d831e06ede
Vera Gonzalez <me@vera.lgbt>
2024-02-15 22:41:24 -0500

    feat: made the oh my zsh install headless

--- dot_config/scripts/fedora/root/executable_terminal.sh
+++ dot_config/scripts/fedora/root/executable_terminal.sh
@@ -8,6 +8,8 @@ fi
 # Install zsh
 dnf install -y git zsh
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Update the default shell
+chsh -s $(which zsh)
 # Exit new shell
 $(exit)
