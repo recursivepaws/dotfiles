0c17cf636e530d5167de39f27df75c6dfcbe293a
Vera Gonzalez <me@vera.lgbt>
2024-04-03 03:43:00 -0400

    fix neotest

--- dot_config/nvim/lua/plugins/debugger.lua
+++ dot_config/nvim/lua/plugins/debugger.lua
@@ -2,6 +2,7 @@ return {
 	-- Framework for testing
 	"nvim-neotest/neotest",
 	dependencies = {
+		"nvim-neotest/nvim-nio",
 		"rouge8/neotest-rust",
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
