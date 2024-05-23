d055c5fffb6ee8b141fc30e567e438bea1ed4656
Vera Gonzalez <me@vera.lgbt>
2024-05-23 15:18:06 -0400

    better cmp and theme

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,9 +1,43 @@
 return {
 	-- Preferred theme
 	{
-		"rebelot/kanagawa.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		config = function()
-			vim.cmd([[colorscheme kanagawa-wave]])
+			vim.cmd.colorscheme("catppuccin")
+			require("catppuccin").setup({
+				flavour = "mocha",
+				custom_highlights = function(C)
+					return {
+						CmpItemKindSnippet = { fg = C.base, bg = C.mauve },
+						CmpItemKindKeyword = { fg = C.base, bg = C.red },
+						CmpItemKindText = { fg = C.base, bg = C.teal },
+						CmpItemKindMethod = { fg = C.base, bg = C.blue },
+						CmpItemKindConstructor = { fg = C.base, bg = C.blue },
+						CmpItemKindFunction = { fg = C.base, bg = C.blue },
+						CmpItemKindFolder = { fg = C.base, bg = C.blue },
+						CmpItemKindModule = { fg = C.base, bg = C.blue },
+						CmpItemKindConstant = { fg = C.base, bg = C.peach },
+						CmpItemKindField = { fg = C.base, bg = C.green },
+						CmpItemKindProperty = { fg = C.base, bg = C.green },
+						CmpItemKindEnum = { fg = C.base, bg = C.green },
+						CmpItemKindUnit = { fg = C.base, bg = C.green },
+						CmpItemKindClass = { fg = C.base, bg = C.yellow },
+						CmpItemKindVariable = { fg = C.base, bg = C.flamingo },
+						CmpItemKindFile = { fg = C.base, bg = C.blue },
+						CmpItemKindInterface = { fg = C.base, bg = C.yellow },
+						CmpItemKindColor = { fg = C.base, bg = C.red },
+						CmpItemKindReference = { fg = C.base, bg = C.red },
+						CmpItemKindEnumMember = { fg = C.base, bg = C.red },
+						CmpItemKindStruct = { fg = C.base, bg = C.blue },
+						CmpItemKindValue = { fg = C.base, bg = C.peach },
+						CmpItemKindEvent = { fg = C.base, bg = C.blue },
+						CmpItemKindOperator = { fg = C.base, bg = C.blue },
+						CmpItemKindTypeParameter = { fg = C.base, bg = C.blue },
+						CmpItemKindCopilot = { fg = C.base, bg = C.teal },
+					}
+				end,
+			})
 		end,
 	},
 	-- Improves notification display
@@ -11,7 +45,7 @@ return {
 		"rcarriga/nvim-notify",
 		lazy = false,
 		config = function()
-			require("notify").setup({ timeout = 50 })
+			require("notify").setup({ timeout = 100, background_colour = "#000000" })
 		end,
 	},
 	-- Improved file search
