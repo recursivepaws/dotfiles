f0c97c5a4f0dcabbf69ee41763d8b0fc5f6a39d1
Vera Gonzalez <me@vera.lgbt>
2024-11-14 12:23:55 -0500

    changed keybind for telescope cmdline

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -38,7 +38,7 @@ return {
 			telescope.setup(opts)
 			telescope.load_extension("cmdline")
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", ":", "<cmd>Telescope cmdline<cr>", {})
+			vim.api.nvim_set_keymap('n', 'Q', ':Telescope cmdline<CR>', { noremap = true, desc = "Cmdline" })
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
