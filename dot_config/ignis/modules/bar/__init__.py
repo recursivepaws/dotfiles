711303c7cadb8c4ac9abdc6cca8e31e18324be26
Vera Gonzalez <me@vera.lgbt>
2024-12-27 10:50:03 -0500

    updated nvim deps; removed layout switcher from ignis

--- dot_config/ignis/modules/bar/__init__.py
+++ dot_config/ignis/modules/bar/__init__.py
@@ -1,6 +1,5 @@
 from ignis.widgets import Widget
 from .workspaces import workspaces
-from .kb_layout import kb_layout
 from .clock import clock
 from .pinned_apps import pinned_apps
 from .tray import tray
@@ -14,12 +13,11 @@ def bar(monitor: int) -> Widget.Window:
         monitor=monitor,
         namespace=f"ignis_BAR_{monitor}",
         layer="top",
-        kb_mode="none",
         child=Widget.CenterBox(
             css_classes=["bar-widget"],
             start_widget=Widget.Box(child=[workspaces()]),
             center_widget=Widget.Box(child=[pinned_apps()]),
-            end_widget=Widget.Box(child=[tray(), kb_layout(), battery_widget(), clock(monitor)]),
+            end_widget=Widget.Box(child=[tray(), battery_widget(), clock(monitor)]),
         ),
         css_classes=["unset"],
     )
