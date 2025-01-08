84a9902d130bb6809cfc2aba328cc0eeb3d9eb4c
Vera Gonzalez <me@vera.lgbt>
2025-01-08 11:13:37 -0500

    fixed eslint OOM by increasing memory limit

---
+++ dot_config/nvim/lua/meovv/plugins/mason-tool-installer.lua
@@ -0,0 +1,13 @@
+return {
+  'WhoIsSethDaniel/mason-tool-installer.nvim',
+  requires = {
+    'williamboman/mason.nvim',
+  },
+  config = function()
+    require('mason-tool-installer').setup({
+      ensure_installed = {
+        'prettierd',
+      },
+    })
+  end,
+}
