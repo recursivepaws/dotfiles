0ba050a01984330bb3463da5ef862219480089e3
Vera Gonzalez <me@vera.lgbt>
2025-01-09 12:43:08 -0500

    mason & mkdn

--- dot_config/nvim/lua/meovv/plugins/markdown.lua
+++ dot_config/nvim/lua/meovv/plugins/markdown.lua
@@ -1,12 +1,8 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		lazy = false,
-	},
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
