f1bc8739c7d7686f2bb796bea0c82c101f80823b
Vera Gonzalez <me@vera.lgbt>
2025-01-09 22:39:46 -0500

    fixed up Comment.nvim and null_ls biome

--- dot_config/nvim/lua/meovv/plugins/comments.lua
+++ dot_config/nvim/lua/meovv/plugins/comments.lua
@@ -1,22 +1,13 @@
 return {
-	"numToStr/Comment.nvim",
-	config = function()
-		require("Comment").setup({
-			pre_hook = function(ctx)
-				local U = require("Comment.utils")
-				local location = nil
-				if ctx.ctype == U.ctype.block then
-					location = require("ts_context_commentstring.utils").get_cursor_location()
-				elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-					location = require("ts_context_commentstring.utils").get_visual_start_location()
-				end
-
-				return require("ts_context_commentstring.internal").calculate_commentstring({
-					key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-					location = location,
-				})
-			end,
-		})
-	end,
-	event = "VeryLazy",
+  "numToStr/Comment.nvim",
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local context_commentstring_integration = require("ts_context_commentstring.integrations.comment_nvim")
+    require("Comment").setup({
+      pre_hook = context_commentstring_integration.create_pre_hook(),
+    })
+  end,
 }
--- dot_config/nvim/lua/meovv/plugins/null-ls.lua
+++ dot_config/nvim/lua/meovv/plugins/null-ls.lua
@@ -2,37 +2,30 @@ local user_config = require("meovv.core.user")
 local u = require("meovv.utils")
 
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-		"gbprod/none-ls-shellcheck.nvim",
-	},
-	config = function()
-		local defaults = require("meovv.lsp.servers.defaults")
-		local null_ls = require("null-ls")
-		local config_opts = {
-			default_cosmic_sources = true,
-		}
-		local function get_user_config_sources()
-			if not config_opts.add_sources then
-				return {}
-			end
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    "gbprod/none-ls-shellcheck.nvim",
+  },
+  config = function()
+    local defaults = require("meovv.lsp.servers.defaults")
+    local null_ls = require("null-ls")
+    local config_opts = {
+      debug = true,
+      sources = {
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.formatting.prettierd.with({
+          env = {
+            PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
+          },
+          extra_filetypes = { "astro" },
+        }),
+        null_ls.builtins.formatting.stylua,
+      },
+    }
 
-			return config_opts.add_sources()
-		end
-
-		config_opts.sources = u.merge_list({
-			null_ls.builtins.code_actions.gitsigns,
-			null_ls.builtins.diagnostics.markdownlint,
-			null_ls.builtins.formatting.prettierd.with({
-				env = {
-					PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
-				},
-				extra_filetypes = { "astro" },
-			}),
-			null_ls.builtins.formatting.stylua,
-		}, get_user_config_sources())
-		null_ls.setup(u.merge(defaults, config_opts))
-	end,
-	event = "BufEnter",
+    null_ls.setup(u.merge(defaults, config_opts))
+  end,
+  event = "BufEnter",
 }
