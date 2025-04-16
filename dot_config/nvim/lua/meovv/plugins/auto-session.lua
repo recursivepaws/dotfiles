b09d90001a51fa294dd9b98d62c4103461b21f4b
Vera Gonzalez <me@vera.lgbt>
2025-04-16 16:28:23 -0400

    delete autosession keybind

--- dot_config/nvim/lua/meovv/plugins/auto-session.lua
+++ dot_config/nvim/lua/meovv/plugins/auto-session.lua
@@ -1,17 +1,19 @@
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 return {
-	"rmagatti/auto-session",
-	lazy = false,
-	keys = {
-		{ "<leader>wr", "<cmd>SessionSearch<cr>", desc = "Session search" },
-		{ "<leader>ws", "<cmd>SessionSave<cr>", desc = "Save session" },
-		{ "<leader>wt", "<cmd>SessionToggleAutoSave<cr>", desc = "Toggle autosave" },
-		{ "<leader>ww", ":w<cr>", desc = "Write buffer" },
-		{ "<leader>wa", ":wall<cr>", desc = "Write all buffers" },
-		{ "<leader>wq", ":wqall<cr>", desc = "Write quit all buffers" },
-	},
-	opts = {
-		suppressed_dirs = { "~/", "~/Documents", "~/Downloads", "/" },
-	},
+  "rmagatti/auto-session",
+  lazy = false,
+  keys = {
+    { "<leader>wr", "<cmd>SessionSearch<cr>",         desc = "Session search" },
+    { "<leader>ws", "<cmd>SessionSave<cr>",           desc = "Save session" },
+    { "<leader>wc", "<cmd>SessionDelete<cr>",         desc = "Delete session" },
+    { "<leader>wt", "<cmd>SessionToggleAutoSave<cr>", desc = "Toggle autosave" },
+    { "<leader>ww", ":w<cr>",                         desc = "Write buffer" },
+    { "<leader>wa", ":wall<cr>",                      desc = "Write all buffers" },
+    { "<leader>wq", ":wqall<cr>",                     desc = "Write quit all buffers" },
+  },
+  opts = {
+    suppressed_dirs = { "~/", "~/Documents", "~/Downloads", "/" },
+    continue_restore_on_error = false,
+  },
 }
