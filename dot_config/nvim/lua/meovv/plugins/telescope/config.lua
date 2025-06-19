b6050ae3e0822ba249805d39d41a170fb2d613d2
Vera Gonzalez <me@vera.lgbt>
2025-06-18 20:23:07 -0400

    more telescoping

--- dot_config/nvim/lua/meovv/plugins/telescope/config.lua
+++ dot_config/nvim/lua/meovv/plugins/telescope/config.lua
@@ -3,129 +3,109 @@ local icons = require("meovv.utils.icons")
 local u = require("meovv.utils")
 
 local default_mappings = {
-  n = {
-    ["Q"] = actions.smart_add_to_qflist + actions.open_qflist,
-    ["q"] = actions.smart_send_to_qflist + actions.open_qflist,
-    ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
-    ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-    ["v"] = actions.file_vsplit,
-    ["s"] = actions.file_split,
-    ["<cr>"] = actions.file_edit,
-  },
+	n = {
+		["Q"] = actions.smart_add_to_qflist + actions.open_qflist,
+		["q"] = actions.smart_send_to_qflist + actions.open_qflist,
+		["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+		["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+		["v"] = actions.file_vsplit,
+		["s"] = actions.file_split,
+		["<cr>"] = actions.file_edit,
+	},
 }
 
 local opts_cursor = {
-  initial_mode = "normal",
-  sorting_strategy = "ascending",
-  layout_strategy = "cursor",
-  results_title = false,
-  layout_config = {
-    width = 0.5,
-    height = 0.4,
-  },
+	initial_mode = "normal",
+	sorting_strategy = "ascending",
+	layout_strategy = "cursor",
+	results_title = false,
+	layout_config = {
+		width = 0.5,
+		height = 0.4,
+	},
 }
 
 local opts_vertical = {
-  initial_mode = "normal",
-  sorting_strategy = "ascending",
-  layout_strategy = "vertical",
-  results_title = false,
-  layout_config = {
-    width = 0.3,
-    height = 0.5,
-    prompt_position = "top",
-    mirror = true,
-  },
+	initial_mode = "normal",
+	sorting_strategy = "ascending",
+	layout_strategy = "vertical",
+	results_title = false,
+	layout_config = {
+		width = 0.3,
+		height = 0.5,
+		prompt_position = "top",
+		mirror = true,
+	},
 }
 
 local opts_flex = {
-  layout_strategy = "flex",
-  layout_config = {
-    width = 0.7,
-    height = 0.7,
-  },
+	layout_strategy = "flex",
+	layout_config = {
+		width = 0.7,
+		height = 0.7,
+	},
 }
 
 require("telescope").setup({
-  defaults = {
-    prompt_prefix = "🔍 ",
-    selection_caret = icons.folder.arrow_closed,
-    file_ignore_patterns = {
-      ".git/",
-      "node_modules/",
-    },
-    dynamic_preview_title = true,
-    vimgrep_arguments = {
-      "rg",
-      "--ignore",
-      "--hidden",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      "--trim",
-    },
-  },
-  -- pickers = {
-  --   buffers = u.merge(opts_flex, {
-  --     prompt_title = "✨ Search Buffers ✨",
-  --     mappings = u.merge({
-  --       n = {
-  --         ["d"] = actions.delete_buffer,
-  --       },
-  --     }, default_mappings),
-  --     sort_mru = true,
-  --     preview_title = false,
-  --   }),
-  --   lsp_code_actions = u.merge(opts_cursor, {
-  --     prompt_title = "Code Actions",
-  --   }),
-  --   lsp_range_code_actions = u.merge(opts_vertical, {
-  --     prompt_title = "Code Actions",
-  --   }),
-  --   lsp_document_diagnostics = u.merge(opts_vertical, {
-  --     prompt_title = "Document Diagnostics",
-  --     mappings = default_mappings,
-  --   }),
-  --   lsp_implementations = u.merge(opts_cursor, {
-  --     prompt_title = "Implementations",
-  --     mappings = default_mappings,
-  --   }),
-  --   lsp_definitions = u.merge(opts_cursor, {
-  --     prompt_title = "Definitions",
-  --     mappings = default_mappings,
-  --   }),
-  --   lsp_references = u.merge(opts_vertical, {
-  --     theme = "dropdown",
-  --     prompt_title = "References",
-  --     mappings = default_mappings,
-  --   }),
-  --   find_files = u.merge(opts_flex, {
-  --     prompt_title = "✨ Search Project ✨",
-  --     mappings = default_mappings,
-  --     hidden = true,
-  --   }),
-  --   diagnostics = u.merge(opts_vertical, {
-  --     mappings = default_mappings,
-  --   }),
-  --   git_files = u.merge(opts_flex, {
-  --     prompt_title = "✨ Search Git Project ✨",
-  --     mappings = default_mappings,
-  --     hidden = true,
-  --   }),
-  --   live_grep = u.merge(opts_flex, {
-  --     prompt_title = "✨ Live Grep ✨",
-  --     mappings = default_mappings,
-  --   }),
-  --   git_branches = u.merge(opts_flex, {
-  --     prompt_title = icons.git.branch .. " Checkout Git Branch " .. icons.git.branch,
-  --     mappings = default_mappings,
-  --   }),
-  --   grep_string = u.merge(opts_vertical, {
-  --     prompt_title = "✨ Grep String ✨",
-  --     mappings = default_mappings,
-  --   }),
-  -- },
+	defaults = {},
+	-- pickers = {
+	--   buffers = u.merge(opts_flex, {
+	--     prompt_title = "✨ Search Buffers ✨",
+	--     mappings = u.merge({
+	--       n = {
+	--         ["d"] = actions.delete_buffer,
+	--       },
+	--     }, default_mappings),
+	--     sort_mru = true,
+	--     preview_title = false,
+	--   }),
+	--   lsp_code_actions = u.merge(opts_cursor, {
+	--     prompt_title = "Code Actions",
+	--   }),
+	--   lsp_range_code_actions = u.merge(opts_vertical, {
+	--     prompt_title = "Code Actions",
+	--   }),
+	--   lsp_document_diagnostics = u.merge(opts_vertical, {
+	--     prompt_title = "Document Diagnostics",
+	--     mappings = default_mappings,
+	--   }),
+	--   lsp_implementations = u.merge(opts_cursor, {
+	--     prompt_title = "Implementations",
+	--     mappings = default_mappings,
+	--   }),
+	--   lsp_definitions = u.merge(opts_cursor, {
+	--     prompt_title = "Definitions",
+	--     mappings = default_mappings,
+	--   }),
+	--   lsp_references = u.merge(opts_vertical, {
+	--     theme = "dropdown",
+	--     prompt_title = "References",
+	--     mappings = default_mappings,
+	--   }),
+	--   find_files = u.merge(opts_flex, {
+	--     prompt_title = "✨ Search Project ✨",
+	--     mappings = default_mappings,
+	--     hidden = true,
+	--   }),
+	--   diagnostics = u.merge(opts_vertical, {
+	--     mappings = default_mappings,
+	--   }),
+	--   git_files = u.merge(opts_flex, {
+	--     prompt_title = "✨ Search Git Project ✨",
+	--     mappings = default_mappings,
+	--     hidden = true,
+	--   }),
+	--   live_grep = u.merge(opts_flex, {
+	--     prompt_title = "✨ Live Grep ✨",
+	--     mappings = default_mappings,
+	--   }),
+	--   git_branches = u.merge(opts_flex, {
+	--     prompt_title = icons.git.branch .. " Checkout Git Branch " .. icons.git.branch,
+	--     mappings = default_mappings,
+	--   }),
+	--   grep_string = u.merge(opts_vertical, {
+	--     prompt_title = "✨ Grep String ✨",
+	--     mappings = default_mappings,
+	--   }),
+	-- },
 })
--- dot_config/nvim/lua/meovv/plugins/telescope/init.lua
+++ dot_config/nvim/lua/meovv/plugins/telescope/init.lua
@@ -1,10 +1,12 @@
 -- Improved file search
+
+local icons = require("meovv.utils.icons")
 -- Added cmdline via telescope
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    --"jonarrien/telescope-cmdline.nvim",
+    "kdheepak/lazygit.nvim",
     "nvim-lua/popup.nvim",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
@@ -27,10 +29,20 @@ return {
           require("telescope.themes").get_dropdown({ winblend = 10 }),
         },
       },
+
+      prompt_prefix = "🔍 ",
+      selection_caret = icons.folder.arrow_closed,
+      file_ignore_patterns = {
+        ".git/",
+        "node_modules/",
+        "*.ipynb",
+      },
+      dynamic_preview_title = true,
     })
 
     require("telescope").load_extension("ui-select")
     require("telescope").load_extension("fzf")
+    require("telescope").load_extension("lazygit")
 
     -- normal mappings
     local map = require("meovv.utils").map
@@ -41,9 +53,11 @@ return {
     map("n", "<leader>fp", project_files, { desc = "Find project file" })
     map("n", "<leader>fg", project_grep, { desc = "Grep whole project" })
     map("n", "<leader>ff", telescope("find_files"), { desc = "Find file" })
+    map("n", "<leader>fc", telescope("commands"), { desc = "Find commands" })
+    map("n", "<leader>fk", telescope("keys"), { desc = "Find keymappings" })
     map("n", "<leader>fs", telescope("live_grep"), { desc = "Grep string" })
     map("n", "<leader>ft", telescope("treesitter"), { desc = "Grep treesitter" })
-    map("n", "<leader>fh", telescope("help_tags"), { desc = "Find Help" })
+    map("n", "<leader>fh", telescope("help_tags theme=ivy"), { desc = "Find Help" })
     map("n", "<leader>fm", telescope("treesitter symbols=function"), { desc = "Grep methods" })
     map("n", "<leader>fb", telescope("buffers"), { desc = "Find buffer" })
     map("n", "<leader>fw", telescope("grep_string"), { desc = "Grep current word" })
