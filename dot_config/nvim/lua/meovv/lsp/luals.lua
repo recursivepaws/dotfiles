c1f54efee371fc006cc71a5d5a8aa4c18fc2046d
Vera Gonzalez <me@vera.lgbt>
2025-09-10 03:17:18 -0400

    cleanup

--- dot_config/nvim/lua/meovv/lsp/luals.lua
+++ dot_config/nvim/lua/meovv/lsp/luals.lua
@@ -1,39 +1,39 @@
 local mason_bin = require("meovv.utils.lsp").mason_bin
 
 vim.lsp.config("luals", {
-	cmd = { "lua-language-server" },
-	filetypes = { "lua" },
-	root_markers = { ".luarc.json", ".luarc.jsonc" },
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			hint = {
-				enable = true,
-				paramName = "Literal",
-				setType = true,
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = {
-					"vim",
-					"require",
-					"ide",
-				},
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-				},
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+  cmd = { mason_bin .. "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      hint = {
+        enable = true,
+        paramName = "Literal",
+        setType = true,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          "vim",
+          "require",
+          "ide",
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 })
 
 vim.lsp.enable("luals")
--- dot_config/nvim/lua/meovv/plugins/editor.lua
+++
@@ -1,7 +0,0 @@
-return {
-	-- Dev icons
-	{ "ryanoasis/vim-devicons" },
-	-- Indent blank lines
-	-- Better diagnostics, references, quickfixes, telescope results
-	-- Run formatters on save
-}
