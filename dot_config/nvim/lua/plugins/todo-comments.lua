c6d9b49d96516341affc31e4ec32fd95f9c9e135
Vera Gonzalez <me@vera.lgbt>
2026-01-16 12:26:16 -0500

    fixed todo comments

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -18,32 +18,36 @@ local icons = require("utils.icons")
 -- HACK:
 
 -- local dracula = require("").setup()
+local palette = require("nightfox.palette").load("carbonfox")
+-- print(vim.inspect(palette))
+-- vim.notify(vim.inspect(palette))
+
 return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "Mofiqul/dracula.nvim" },
-	--[[ opts = {
-		colors = {
-			error = { dracula.red },
-			warning = { dracula.orange },
-			info = { dracula.cyan },
-			hint = { dracula.green },
-			default = { dracula.purple },
-			refactor = { dracula.pink },
-		},
-		keywords = {
-			FIX = {
-				icon = icons.debug, -- icon used for the sign, and in search results
-				color = "error", -- can be a hex color, or a named color (see below)
-				alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "fix", "fixme", "bug" }, -- a set of other keywords that all map to this FIX keywords
-			},
-			TODO = { icon = icons.check, color = "info" },
-			DOCS = { icon = icons.file, color = "hint" },
-			HACK = { icon = icons.flame, color = "warning" },
-			WARN = { icon = icons.warn, color = "warning", alt = { "WARNING", "XXX" } },
-			PERF = { icon = icons.clock, alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-			NOTE = { icon = icons.comment, color = "hint" },
-			INFO = { icon = icons.info, color = "hint" },
-			REFACTOR = { icon = icons.hint, color = "refactor" },
-		},
-	}, ]]
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "EdenEast/nightfox.nvim" },
+  opts = {
+    colors = {
+      error = { palette.red.base },
+      warning = { palette.orange.base },
+      info = { palette.cyan.base },
+      hint = { palette.green.base },
+      default = { palette.blue.base },
+      refactor = { palette.pink.base },
+    },
+    keywords = {
+      FIX = {
+        icon = icons.debug,                                            -- icon used for the sign, and in search results
+        color = "error",                                               -- can be a hex color, or a named color (see below)
+        alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "fix", "fixme", "bug" }, -- a set of other keywords that all map to this FIX keywords
+      },
+      TODO = { icon = icons.check, color = "info" },
+      DOCS = { icon = icons.file, color = "hint" },
+      HACK = { icon = icons.flame, color = "warning" },
+      WARN = { icon = icons.warn, color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = icons.clock, alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = icons.comment, color = "hint" },
+      INFO = { icon = icons.info, color = "hint" },
+      REFACTOR = { icon = icons.hint, color = "refactor" },
+    },
+  },
 }
